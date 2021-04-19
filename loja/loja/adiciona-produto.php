53b5a2963c03d85323b1b43f0f14562deb91e487
diogo535 <61697081+diogo535@users.noreply.github.com>
2021-04-19 12:21:11 +0100

    Add files via upload

---
+++ loja/loja/adiciona-produto.php
@@ -0,0 +1,23 @@
+<?php include("cabecalho.php"); ?>
+    <?php
+    $nome = $_GET["nome"];
+    $preco = $_GET["preco"];
+    
+    $query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+    
+    $conexao = mysqli_connect('localhost', 'root', '', 'loja');
+    if(mysqli_query($conexao, $query)) {
+    ?>
+        <p class="alert-success">
+            Produto: <?= $nome; ?>, preco: <?= $preco; ?> - adicionado com sucesso!
+        </p>
+    <?php
+    } else {
+    ?>
+        <p class="alert-danger">
+            ERRO - O produto não foi adicionado!
+        </p>
+    <?php
+    }
+    ?>
+<?php include("rodape.php"); ?>
---
+++ loja/loja/produto-formulario.php
@@ -0,0 +1,10 @@
+<?php include("cabecalho.php"); ?>
+    
+    <h1>Formulário de Cadastro</h1>
+    <form action="adiciona-produto.php">
+        Nome: <input type="text" name="nome" /><br/><br/>
+        Preço: <input type="number" name="preco" /><br/><br/>
+        <input type="submit" value="Cadastrar" />
+    </form>
+
+<?php include("rodape.php"); ?>
